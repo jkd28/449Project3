a9809978d1d83368d93161ddf0763e0f48c9275d
John Dott <jkd28@pitt.edu>
2017-03-19 16:28:57 -0400

    Added some functionality

--- mymalloc.c
+++ mymalloc.c
@@ -20,21 +20,27 @@ void *my_bestfit_malloc(int size){
 
 	// check empty status of Linked list
 	if(head == NULL){
-		printf("Adding Head\n");
 		//if empty, set new memory location to the head/tail node
-		struct Node *new_node = {size, 1, NULL, NULL};	//create a new node for the pointer of allocated space
-		head = new_node;
-		tail = head;
+		printf("Adding Head\n");
 		//allocate space on the heap
 		brk = sbrk(size+sizeof(struct Node));
-		return (char *)brk + sizeof(struct Node); // return the value of the new memory space to be used by calling function
+		struct Node *new_node = (struct Node *)brk+1;	//create a new node in allocated space
+		new_node->size = size+sizeof(struct Node);
+		new_node->occupied = 1;
+		new_node->next = NULL;
+		new_node->previous = NULL;
+		head = new_node;
+		tail = head;
+		return brk + sizeof(struct Node); // return the value of the new memory space to be used by calling function
 	}else {
+		int count = 0;
 		printf("Adding other\n");
 		// search for best fit node
 		struct Node *best = NULL;
-		struct Node *current_node = &head; //start traversal at the head
-
-		while(&current_node != NULL){	// while we have no best choice
+		struct Node *current_node = head; //start traversal at the head
+		//size = size + sizeof(struct Node);
+		while(current_node != NULL){	// while we have no best choice
+			printf("Stuck %d\n", ++count);
 			if (size == current_node->size){
 				best = current_node;  //immediate best fit
 				break;
@@ -48,18 +54,36 @@ void *my_bestfit_malloc(int size){
 		//best does not exist, create new node and allocate more space
 		if(best == NULL){
 			brk = sbrk(size+sizeof(struct Node)); //allocate all space on heap
-			
+			struct Node *new_node = (struct Node *)brk +1;
+			//create new node with space allocated
+			new_node->size = size+sizeof(struct Node);
+			new_node->occupied = 1;
+			new_node->next = NULL;
+			new_node->previous = tail;
+			tail = new_node;
+			return (new_node + sizeof(struct Node));
+		} else {
+			//we have a best fit in empty space
+			best->occupied=1;
+			return (best + sizeof(struct Node));
 		}
 	}
 }
 
 void my_free(void *ptr){
-	struct Node *node = ptr - sizeof(struct Node); // get the node that points to this memory section
-	void * brk = sbrk(0); //check current value of brk
+	struct Node *current = head;	//get head node
+	struct Node *node = ptr - sizeof(struct Node); // get the pointer to the node in memory section
+	void *brk = sbrk(0); //check current value of brk
 	int size = node->size;
 	//find node in LL
+	while(current != node){
+		current = current->next;
+	}
 	//delete node
-		//check previous and next for free space and coalesce if necessary
+	(current->next)->previous = current->previous;
+	(current->previous)->next = current->next;
+	//check previous and next for free space and coalesce if necessary
+
 	//free space of that size if that node touched brk
 	if(node+size == brk){
 		size = -1 * size;
--- test.c
+++ test.c
@@ -3,9 +3,13 @@
 #include "mymalloc.h"
 
 int main(){
+  printf("%p\n", sbrk(0));
+  //allocate some space
   void *new_brk = my_bestfit_malloc(1);
   void *try_again = my_bestfit_malloc(30);
-  printf("%p\n", new_brk);
+  //print new value of brk
+  printf("%p\n", sbrk(0));
   my_free(new_brk);
   printf("Successful Free\n");
+  my_free(try_again);
 }
